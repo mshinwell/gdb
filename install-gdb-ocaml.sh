d932dc52d80724c7b87eb8a6105941d0a9cca98f
Mark Shinwell <mshinwell@janestreet.com>
2016-10-03 13:52:36 +0100

    fixes to install script

--- install-gdb-ocaml.sh
+++ install-gdb-ocaml.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+
 set -eu
 target_bindir=$1
 gdb_install_root=$2
 libmonda_install_root=$3
+
 mkdir -p $target_bindir
 cat > $target_bindir/gdb-ocaml << EOF
 #!/bin/sh
@@ -11,3 +13,10 @@ export OCAMLRUNPARAM=b
 $gdb_install_root/bin/gdb "\$@"
 EOF
 chmod +x $target_bindir/gdb-ocaml
+
+for header in $(find . -name "*.h"); do
+  dirname=$(dirname "$header")
+  install_dirname=$gdb_install_root/src/$(dirname)
+  mkdir -p $install_dirname
+  cp $header $install_dirname
+done
