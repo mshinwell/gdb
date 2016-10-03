9abc76eec2ff9b2c06cd13f1431ce49ab9d04c20
Mark Shinwell <mshinwell@janestreet.com>
2016-10-03 13:39:58 +0100

    escape

--- install-gdb-ocaml.sh
+++ install-gdb-ocaml.sh
@@ -6,7 +6,7 @@ libmonda_install_root=$3
 mkdir -p $target_bindir
 cat > $target_bindir/gdb-ocaml << EOF
 #!/bin/sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$libmonda_install_root
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$libmonda_install_root
 export OCAMLRUNPARAM=b
 $gdb_install_root/bin/gdb "$@"
 EOF
